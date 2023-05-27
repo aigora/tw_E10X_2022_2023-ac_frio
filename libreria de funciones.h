2251b1976e3d863eb9d4773b09f27c1bf8b133bd
jsdefrancisco <79968624+jsdefrancisco@users.noreply.github.com>
2023-05-27 18:33:05 +0200

    actualizacion libreria de funciones.h
    actualizacion final de la estructura emplea, y las diversas funciones

--- libreria de funciones.h
+++ libreria de funciones.h
@@ -1,40 +1,44 @@
-typedef struct     //estructura para el nombre del tipo de energia
+//////////////// Libreria con las cabezeras de las funciones y las estructuras
+
+
+typedef struct    //estructura para el nombre del tipo de energia
 {
-    char tipo[50];
-    int fecha;
-    float generacion[50];
+    int mes;
+	int ano;
+	double Hidraulica; //RENOVABLE
+	double Turbinacion_bombeo; //RENOVABLE
+	double Nuclear; //RENOVABLE
+	double Carbon; //NO RENOVABLE
+	char Fuel_gasc; 
+	double Fuel_gas; //NO RENOVABLE
+	double Motores_diesel; //NO RENOVABLE
+	double Turbina_gas; //MEZCLA
+	double Turbina_vapor; //MEZCLA
+	double Ciclo_combinado; //MEZCLA
+	double Hidroeolica; //RENOVABLE
+	double Eolica; //RENOVABLE
+	double Solar_fotovoltaica; //RENOVABLE
+	double Solar_termica; //RENOVABLE
+	double Otras_renovables; //RENOVABLE
+	double Cogeneracion; //MEZCLA
+	double Residuos_no_renovables; //RESIDUOS
+	double Residuos_renovables; //RESIDUOS
+	double Generacion_total; 
+}datos;
 
- 
 
-}energia;
-
- 
-
- 
 
 
 void menu_principal(int *opcion);       //funcion del menu principal
 
- 
-
 void informacion(int *info);            //funcion para la informacion sobre el programa
 
- 
+void menu_secundario(int *seleccion, datos valores[]);   //funcion menu secundario
 
-void menu_secundario(int *seleccion);   //funcion menu secundario
+int apertura_lectura_guardardatos(datos valores[], int n);     //funcion comprobar fichero y lectura de datos
 
- 
+void diversos_calculos(datos valores[], int n);               //funcion realiza los calculos
 
-int comprobar_existencia_fichero();     //funcion comprobar fichero y lectura de datos
+void cargar_resultados(datos valores[], int n);               //funcion realiza carga de datos
 
- 
-
-void cargar_fichero();
-
- 
-
-int crear_nuevo_fichero();
-
- 
-
-void diversos_calculos();               //funcion realza los calculos
+void busqueda_resultados(datos valores[], int n);               //funcion realiza busqueda de datos
